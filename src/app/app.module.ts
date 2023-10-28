9f84addc4c0e26440dfc7149f2e067db5163e9cb
Seth T <seth10@users.noreply.github.com>
2023-10-28 13:08:38 -0700

    Add a wheel with a notch for the lock

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WheelComponent } from './wheel/wheel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WheelComponent
   ],
   imports: [
     BrowserModule,
